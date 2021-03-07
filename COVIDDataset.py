6c22eaaf3ea28a2a73ceae41cb089f2e88c6a0f9
Jake Atwell <jatwell@alexinc.com>
2021-03-07 13:07:21 -0500

    Reformat with black

--- COVIDDataset.py
+++ COVIDDataset.py
@@ -6,15 +6,18 @@ import torch
 from collections import Counter
 from torch.utils.data import Dataset
 
+
 class COVIDDataset(Dataset):
     def __init__(self, path, grouping_variables):
-        data=pd.read_csv(path)
+        data = pd.read_csv(path)
         self.data = data
         self.path = os.path.dirname(path)
-        classes = data.apply(lambda x:'_'.join(x[grouping_variables]), axis=1).to_frame(name='factor')
-        classes['idx'] = classes.index
+        classes = data.apply(
+            lambda x: "_".join(x[grouping_variables]), axis=1
+        ).to_frame(name="factor")
+        classes["idx"] = classes.index
         self.classes = classes
-        self.counts = Counter(self.classes['factor'])
+        self.counts = Counter(self.classes["factor"])
         return
 
     def __getitem__(self, i):
@@ -27,8 +30,10 @@ class COVIDDataset(Dataset):
         inputs = []
         labels = []
         for item in batch:
-            audio, sr = sf.read(os.path.join(self.path, 'AUDIO', item['File_name']+".flac"))
-            status = 1 if item['Covid_status'] == 'p' else 0
+            audio, sr = sf.read(
+                os.path.join(self.path, "AUDIO", item["File_name"] + ".flac")
+            )
+            status = 1 if item["Covid_status"] == "p" else 0
             inputs.append(audio)
             labels.append(status)
-        return (inputs, torch.unsqueeze(torch.tensor(labels),0))
+        return (inputs, torch.unsqueeze(torch.tensor(labels), 0))
